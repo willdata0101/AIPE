8c31bf853d15dc7eeb4238769a67c43cbcc25af2
William <willthelion@outlook.com>
2024-11-19 19:05:54 -0600

    Fixed stuff

--- MHAA.py
+++ MHAA.py
@@ -99,14 +99,16 @@ def run_assistant():
                 submitted = st.form_submit_button("Submit")
     
     if submitted:
+        message = st.chat_message("assistant")
+        message.write("Researching your question...")
         assistant = Assistant(bedrock_client)
         # Handle the query
         response = assistant.handle_query(text)
         clean_response = response['answer']
         clean_response = "".join(clean_response)
         st.write(clean_response)
-    # else:
-    #     print()
+    else:
+        print()
     st.rerun()
 
 run_assistant()
