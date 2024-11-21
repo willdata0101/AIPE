dab382b50489f397821b624f08db78b4d5752444
William <willthelion@outlook.com>
2024-11-21 15:35:32 -0600

    Deleted files

--- wiki_test.py
+++
@@ -1,8 +0,0 @@
-import wikipedia
-
-try:
-    term = "WWII"  # Ensure this is not empty or None
-    results = wikipedia.search(term)
-    print(results)
-except Exception as e:
-    print(f"An error occurred: {e}")
